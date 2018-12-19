6c88693a760cf9fb44b5fe62572bbf7ccbfd2325
Avi Stramer <astramer@gmail.com>
2018-12-19 10:53:49 -0600

    Update testable-monitor.sh

--- testable-monitor.sh
+++ testable-monitor.sh
@@ -4,12 +4,12 @@
 
 execution_id=$1
 echo "[$(date)] Waiting for execution to complete (view online at https://a.testable.io/results/$execution_id)"
-while [ $(curl -H "X-Testable-Key:$TESTABLE_KEY" --silent https://dev.testable.io:8080/executions/$execution_id | jq -r ".completed") = "false" ]; do
+while [ $(curl -H "X-Testable-Key:$TESTABLE_KEY" --silent https://api.testable.io/executions/$execution_id | jq -r ".completed") = "false" ]; do
   echo -n "."
   sleep 5
 done
 
-if [ $(curl -H "X-Testable-Key:$TESTABLE_KEY" --silent https://dev.testable.io:8080/executions/$execution_id | jq -r ".success") = "false" ]; then
+if [ $(curl -H "X-Testable-Key:$TESTABLE_KEY" --silent https://api.testable.io/executions/$execution_id | jq -r ".success") = "false" ]; then
   echo -e "\n[$(date)] Test FAILED"
 else 
   echo -e "\n[$(date)] Test SUCCESS"
@@ -17,4 +17,4 @@ fi
 
 epoch=$(date +"%s")
 echo "[$(date)] Storing CSV results at results-$epoch.csv"
-curl -H "X-Testable-Key:$TESTABLE_KEY" --silent https://dev.testable.io:8080/executions/$execution_id/results.csv > results-$epoch.csv
+curl -H "X-Testable-Key:$TESTABLE_KEY" --silent https://api.testable.io/executions/$execution_id/results.csv > results-$epoch.csv
