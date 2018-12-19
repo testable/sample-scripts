dbbccc72ca7a1fa82502a6b6001c0ac4649b543a
Avi Stramer <astramer@gmail.com>
2018-12-19 10:53:18 -0600

    testable-monitor.sh

---
+++ testable-monitor.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Takes as input a test execution ID to monitor
+
+execution_id=$1
+echo "[$(date)] Waiting for execution to complete (view online at https://a.testable.io/results/$execution_id)"
+while [ $(curl -H "X-Testable-Key:$TESTABLE_KEY" --silent https://dev.testable.io:8080/executions/$execution_id | jq -r ".completed") = "false" ]; do
+  echo -n "."
+  sleep 5
+done
+
+if [ $(curl -H "X-Testable-Key:$TESTABLE_KEY" --silent https://dev.testable.io:8080/executions/$execution_id | jq -r ".success") = "false" ]; then
+  echo -e "\n[$(date)] Test FAILED"
+else 
+  echo -e "\n[$(date)] Test SUCCESS"
+fi
+
+epoch=$(date +"%s")
+echo "[$(date)] Storing CSV results at results-$epoch.csv"
+curl -H "X-Testable-Key:$TESTABLE_KEY" --silent https://dev.testable.io:8080/executions/$execution_id/results.csv > results-$epoch.csv
